4bd5f090ffdf4ab956d09858ee2b53a01f5e6a36
yamadeey <daiinzm1130@icloud.com>
2021-07-07 19:38:58 +0900

    レビュー修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,9 +12,10 @@ class Item < ApplicationRecord
     validates :title, :description, :image
   end
 
-  with_options numericality: {oter_than: 1 } do
+  with_options numericality: {other_than: 1 } do
     validates :burden_id, :category_id, :prefecture_id, :delivery_id, :status_id
   end
+
   with_options format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: {greater_than: 300, less_than: 9999999 }
   end
