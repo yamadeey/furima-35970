b370b56e5e24acdb5be660c8a584aea3099e79c5
yamadeey <daiinzm1130@icloud.com>
2021-07-08 15:31:22 +0900

    レビュー修正２

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     title         {Faker::Name.initials}
     description   {Faker::Lorem.sentence}
-    category_id     {'2'}
-    status_id       {'2'}
-    burden_id       {'3'}
-    prefecture_id   {'3'}
-    delivery_id     {'2'}
-    price           {'1000'} 
+    category_id     {2}
+    status_id       {2}
+    burden_id       {3}
+    prefecture_id   {3}
+    delivery_id     {2}
+    price           {1000} 
     association :user
 
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -30,30 +30,30 @@ describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-        it 'category_idが空では登録されない' do
-        @item.category_id = nil
+        it 'category_idが1では登録されない' do
+        @item.category_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category is not a number")
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
-        it 'status_idが空では登録されない' do
-        @item.status_id = nil
+      it 'status_idが1では登録されない' do
+        @item.status_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status is not a number")
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
-        it 'burden_idが空では登録されない' do
-        @item.burden_id = nil
+        it 'burden_idが1では登録されない' do
+        @item.burden_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Burden is not a number")
+        expect(@item.errors.full_messages).to include("Burden must be other than 1")
       end
-      it 'prefecture_idが空では登録されない' do
-        @item.prefecture_id = nil
+      it 'prefecture_idが1では登録されない' do
+        @item.prefecture_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture is not a number")
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
-      it 'delivery_idが空では登録されない' do
-        @item.delivery_id = nil
+      it 'delivery_idが1では登録されない' do
+        @item.delivery_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery is not a number")
+        expect(@item.errors.full_messages).to include("Delivery must be other than 1")
       end
       it 'priceが空では登録されない' do
         @item.price = ""
@@ -65,13 +65,18 @@ describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it 'priceが半角英数混合では登録できない' do
+        @item.price = "1000a"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
       it 'priceが300より少ないと登録できない' do
-        @item.price = "50"
+        @item.price = 50
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than 300")
       end
       it 'priceが9999999より多いと登録できない' do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than 9999999")
       end
